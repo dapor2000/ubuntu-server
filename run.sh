08bd3c6bbfcff3e019ff4658466d4a2400791dac
dapor2000 <dapor@dapor.de>
2016-10-12 14:38:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-#/usr/sbin/a2enmod *
-#/usr/sbin/a2dismod lua
-#cd /etc/apache2/site-available
-#a2ensite *
-#cd
-#echo "RESTART APACHE"
-#/etc/init.d/apache2 restart
-#echo "START TRACCAR"
-#/opt/traccar/bin/traccar start &
+updatedb
+rm /etc/apache2/sites-available/default-ssl.conf                                                                                          
+rm /etc/apache2/sites-enabled/default-ssl.conf 
+cd /etc/apache2/mods-available/
+/usr/sbin/a2enmod *
+/usr/sbin/a2dismod lua
+cd /etc/apache2/sites-available
+a2ensite *
+cd
+echo "RESTART APACHE"
+/etc/init.d/apache2 restart
+echo "START TRACCAR"
+/opt/traccar/bin/traccar start &
+echo "START SSHD"
 /usr/sbin/sshd -D
